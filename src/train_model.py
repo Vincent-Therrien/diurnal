60168b31f97bbb3593cf439e0c1f774efaec3e31
Vincent Therrien <vincentstherrien@gmail.com>
2023-03-11 21:39:19 -0500

    Validation croisée

--- src/train_model.py
+++ src/train_model.py
@@ -5,25 +5,26 @@ from torch import nn
 import torch
 from tqdm import tqdm
 
-import cnn.network as cnn
 import utils.datahandler as utils
+import networks.cnn as cnn
 
-# Set working directory to the location of the script.
+# Set working directory to the location of the script to retrieve files.
 import os
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
-
 formatted_path = "../data/archiveII-arrays/"
 
 # Parameters
 batch_size = 32
 device = "cuda" if torch.cuda.is_available() else "cpu"
+family = "5s"
 
 # Functions
-def train(model, dataloader, optimizer, loss_fn, epochs, validation = None):
+def train(model, dataloader, optimizer, loss_fn, epochs, validation=None,
+        verbose=True):
     model.train()
-    for _ in tqdm(range(epochs)):
+    for epoch in tqdm(range(epochs)) if verbose else range(epochs):
         for batch, (x, y) in enumerate(dataloader):
             optimizer.zero_grad()
             pred = model(x)
@@ -31,12 +32,15 @@ def train(model, dataloader, optimizer, loss_fn, epochs, validation = None):
             loss.backward()
             optimizer.step()
             loss = loss.item(), (batch + 1) * len(x)
-        if validation:
-            test(model, validation)
+            if verbose and batch % 10 == 0:
+                print(f"Loss: {loss}")
+        if verbose and validation:
+            _, _, f1 = test(model, validation)
+            print(f"Epoch {epoch} validation score: {f1:.4}")
 
 def test(model, dataloader):
     model.eval()
-    metrics = [[], [], []]
+    sensitivity, ppv, f1 = [], [], []
     with torch.no_grad():
         for x, y in dataloader:
             x, y = x.to(device), y.to(device)
@@ -45,36 +49,47 @@ def test(model, dataloader):
                 s, p, f = utils.get_evaluation_metrics(
                     utils.prediction_to_secondary_structure(i.tolist()),
                     utils.prediction_to_secondary_structure(j.tolist()))
-                metrics[0].append(s)
-                metrics[1].append(p)
-                metrics[2].append(f)
-    print(f"Sensitivity: {np.mean(metrics[0]):.4f}    ", end="")
-    print(f"PPV: {np.mean(metrics[1]):.4f}    ", end="")
-    print(f"F1-score: {np.mean(metrics[2]):.4f}")
+                sensitivity.append(s)
+                ppv.append(p)
+                f1.append(f)
+    return np.mean(sensitivity), np.mean(ppv), np.mean(f1)
+
+def k_fold_benchmark(model, data, K, loss_fn, optimizer, n_epochs,
+        verbose=True) -> tuple:
+    sensitivity, ppv, f1 = [], [], []
+    for k in range(K):
+        # Split the data for the current fold.
+        training_data, validation_data, test_data = (
+            utils.k_fold_split(data, [0.8, 0.1, 0.1], K, k))
+        train_dataloader = DataLoader(training_data, batch_size=batch_size)
+        valid_dataloader = DataLoader(validation_data, batch_size=batch_size)
+        test_dataloader = DataLoader(test_data, batch_size=batch_size)
+        # Train the model with the current set of data.
+        train(model, train_dataloader, optimizer, loss_fn, n_epochs,
+            valid_dataloader, verbose)
+        # Collect performance metric for the current fold.
+        s, p, f = test(model, test_dataloader)
+        sensitivity.append(s)
+        ppv.append(p)
+        f1.append(f)
+    return np.mean(sensitivity), np.mean(ppv), np.mean(f1)
+
+def load_data(family):
+    x = np.load(formatted_path + family + "_x.npy")
+    y = np.load(formatted_path + family + "_y.npy")
+    data = []
+    for i in range(len(x)):
+        data.append([
+            torch.tensor(x[i].T, dtype=torch.float32).to(device),
+            torch.tensor(y[i], dtype=torch.float32).to(device)])
+    return data
 
 # Usage
-print("Loading data")
-x = np.load(formatted_path + "5s_x.npy")
-y = np.load(formatted_path + "5s_y.npy")
+data = load_data(family)
+rna_length = len(data[0][0].T)
 
-print("Splitting data")
-data = []
-for i in range(len(x)):
-    data.append([
-        torch.tensor(x[i].T, dtype=torch.float32).to(device),
-        torch.tensor(y[i], dtype=torch.float32).to(device)])
-
-training_data, validation_data, test_data = utils.k_fold_split(data, [0.8, 0.1, 0.1], 4, 0)
-
-# Create data loaders.
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-validation_dataloader = DataLoader(validation_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-print("Beginning the training")
-model = cnn.convolutionalNN(len(x[0])).to(device)
+model = cnn.convolutionalNN(rna_length).to(device)
 optimizer = optim.Adam(model.parameters())
 loss_fn = nn.CrossEntropyLoss()
 
-train(model, train_dataloader, optimizer, loss_fn, 5, validation_dataloader)
-test(model, test_dataloader)
+print(k_fold_benchmark(model, data, 5, loss_fn, optimizer, 5, False))
